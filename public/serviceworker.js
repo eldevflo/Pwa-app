b49e3eaacdb351a265cbcf9b8a5e9c02ebfac35a
fatemeh zamanipour <f.zamanipour@gmail.com>
2022-01-11 19:52:19 +0330

    updated

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,43 +1,53 @@
-const CACHE_NAME = 'version-1';
-const urlsToCache = ['index.html', 'offline.html']
-
-const self = this;
-//install sw 
-self.addEventListener('install' , (event)=>{
-   event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    )
-})
-//Listen for request
-self.addEventListener('fetch' , (event)=>{
-    //cash all requests
-    //if there is'nt internet connection display offline.html
-   event.respondWith(
-        caches.match(event.request)
-            .then(() => {
-                return fetch(event.request) 
-                    .catch(() => caches.match('offline.html'))
-            })
-    )
-})
-//activate the sw
-self.addEventListener('activate' , (event)=>{
-    //if there is changes in data remove the prev data 
-    const cacheWhitelist = [];
-    cacheWhitelist.push(CACHE_NAME);
+let cacheData = 'appV-1';
+let dynamicCacheName = 'site-dynamicCach-v1'
+let self = this;
+//cache size limit function
+const limitCacheSize = (name , size) => {
+    caches.open(name).then(cache =>{
+        cache.keys().then(keys=>{
+            if(keys.length > size){
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        })
+    })
+};
+self.addEventListener('install' , (event) => {
     event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
-                if(!cacheWhitelist.includes(cacheName)) {
-                    return caches.delete(cacheName);
+        caches.open(cacheData).then(catche => {
+            catche.addAll([
+                'static/js/main.chunk.js',
+                'static/js/0.chunk.js',
+                'static/js/bundle.js',
+                '/index.html',
+                '/',
+            ]);
+        })
+    )
+});
+self.addEventListener('fetch' , (event) => {
+     if (!(event.request.url.indexOf('http') === 0)) return; 
+        event.respondWith(
+            caches.match(event.request).then((catchResp)=>{
+                return catchResp || fetch(event.request).then(fetchRes=>{
+                    return caches.open(dynamicCacheName).then(cache =>{
+                        cache?.put(event.request.url , fetchRes.clone());
+                        limitCacheSize(dynamicCacheName, 15);
+                        return fetchRes;
+                    })
+                })
+            }).catch(() => {
+                if(event.request.url.indexOf('.html') > -1){
+                return caches.match('/')
                 }
             })
-        ))
-            
+        );
+    });
+self.addEventListener('activate' , (event) => {
+    event.waitUntil(
+        caches.keys().then(keys =>{
+            return Promise.all(keys.filter(key => key !== cacheData && key !== dynamicCacheName))
+                .map(key => caches.delete(key))
+        })
     )
-    
-})
+   
+});
